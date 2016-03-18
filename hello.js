b00b14add5ce545db4326897d9a4f3fe4df615e5
amippy <aminaka1223@gmail.com>
2016-03-18 19:43:53 +0900

    配列・演算子

--- hello.js
+++ hello.js
@@ -2,3 +2,12 @@ document.writeln('Muscle,World');
 var msg = 'こんにちは';
 document.writeln(msg);
 
+var array = ['Ami','Ykevi','Valt']
+document.writeln(array[1]);
+
+var obj = {x:1, y:2, z:3};
+document.writeln(obj.x);
+
+var hoge = ['a','b','c'];
+var ho = hoge; 
+document.writeln(ho);
