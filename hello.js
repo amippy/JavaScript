b628e88b04797db7cd892e640f23e32e4d12d852
amippy <aminaka1223@gmail.com>
2016-03-22 19:50:13 +0900

    ループ文

--- hello.js
+++ hello.js
@@ -6,34 +6,21 @@ document.writeln(msg);
 var array = ['ami','amigo','amippy'];
 document.writeln(array[2]);
 
+var x = 10;
+while(x < 10){
+  document.writeln('xの値は'+x);
+  x++
+}
+
+var x = 10;
+do{
+
+  document.writeln('xの値は'+x);
+  x++
+}while(x < 10);
+
 var x = 20;
-if(x >= 10){
-  document.writeln('変数xは１０以上です。');
-}else{
-  document.writeln('変数xは１０以下です。')
-}
-
-var y = 100;
-if(y > 50){
-  document.writeln('yは50より大きい');
-}else{
-  document.writeln('yは50より小さい');
-}
-
-var rank = 'O';
-switch (rank) {
-  case 'A':
-  document.writeln('Aランク');
-  break;
-
-  case 'B':
-  document.writeln('Bランク');
-  break;
-
-  case 'C':
-  document.writeln('Cランクです');
-  break;
-
-  default:
-  document.writeln('ランク外です。。。。')
-}
+do{
+  document.writeln('xの値は' + x);
+  x++
+}while(x < 100);
