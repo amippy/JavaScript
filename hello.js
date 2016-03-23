25387576dfe0d9dfd79533a78c6133e0fb7c4762
amippy <aminaka1223@gmail.com>
2016-03-23 18:30:20 +0900

    昨日の復習

--- hello.js
+++ hello.js
@@ -1,26 +1,51 @@
-document.writeln('Hey');
+document.writeln('HelloWorld');
 
-var msg = "yahoo!hehe";
+
+var msg = 'What da fuck are you doing!'
 document.writeln(msg);
 
-var array = ['ami','amigo','amippy'];
-document.writeln(array[2]);
-
 var x = 10;
-while(x < 10){
-  document.writeln('xの値は'+x);
-  x++
+if(x > 5){
+  document.writeln('変数xは５より大きい数！');
+}else{
+  document.writeln('変数xは５より小さい数！');
+}
+
+var signal = 'red';
+switch(signal){
+  case 'red':
+  document.writeln('渡ってはいけません！');
+  break;
+
+  case 'yellow':
+  document.writelm('危ないですよ！');
+  break;
+
+  case 'blue':
+  document.writeln('渡ってください！');
+  break;
 }
 
 var x = 10;
-do{
+while(x < 20){
+  document.writeln('xの値は'+ x);
+  x++;
+}
 
-  document.writeln('xの値は'+x);
-  x++
-}while(x < 10);
-
-var x = 20;
+var fuck = 20;
 do{
+  document.writeln(fuck + '回目のfuck');
+  fuck++;
+}while(fuck < 30);
+
+x = 10;
+while(x < 20){
   document.writeln('xの値は' + x);
-  x++
-}while(x < 100);
+  x++;
+}
+
+x = 3;
+do{
+  document.writeln('xの値は' + x)
+  x++;
+}while(x < 5);
