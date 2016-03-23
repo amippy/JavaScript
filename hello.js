803b95f3c2af26efbcf9f187ffb8cb6e3aacae3c
amippy <aminaka1223@gmail.com>
2016-03-23 18:51:33 +0900

    for文

--- hello.js
+++ hello.js
@@ -1,51 +1,7 @@
-document.writeln('HelloWorld');
-
-
-var msg = 'What da fuck are you doing!'
-document.writeln(msg);
-
-var x = 10;
-if(x > 5){
-  document.writeln('変数xは５より大きい数！');
-}else{
-  document.writeln('変数xは５より小さい数！');
+for(var i = 10, j=20; i < 30; i++ , j++){
+  document.writeln('iの値は'+ i , 'jの値は'+j);
 }
 
-var signal = 'red';
-switch(signal){
-  case 'red':
-  document.writeln('渡ってはいけません！');
-  break;
-
-  case 'yellow':
-  document.writelm('危ないですよ！');
-  break;
-
-  case 'blue':
-  document.writeln('渡ってください！');
-  break;
+for(var i = 20; i < 30; i++){
+  document.writeln('iの値は' + i);
 }
-
-var x = 10;
-while(x < 20){
-  document.writeln('xの値は'+ x);
-  x++;
-}
-
-var fuck = 20;
-do{
-  document.writeln(fuck + '回目のfuck');
-  fuck++;
-}while(fuck < 30);
-
-x = 10;
-while(x < 20){
-  document.writeln('xの値は' + x);
-  x++;
-}
-
-x = 3;
-do{
-  document.writeln('xの値は' + x)
-  x++;
-}while(x < 5);
